f46c7c13c9ba1dda8a97db118978fbc42780ec60
Ralph Spencer Lacson <ralphlacs308@gmail.com>
2024-02-25 02:16:18 +0800

    Contact Us Form Design

--- client/src/pages/contactpage/ContactPage.jsx
+++ client/src/pages/contactpage/ContactPage.jsx
@@ -8,11 +8,22 @@ const ContactPage = () => {
     <section>
       <Banner page='Contact Us!' prevPage={[{page: 'Home', to: '/' }]} />
 
-      <div className='flex flex-col md:flex-row px-8 md:px-4 lg:px-28 xl:px-58 2xl:px-64'>
-        <div className='w-1/2'>
-          
+      <div className='flex flex-col md:flex-row px-8 py-16 md:px-4 lg:px-28 xl:px-58 2xl:px-64'>
+        <div className='w-full md:w-1/2 p-8'>
+          <h1 className='text-black text-2xl font-bold'>Send us a Message!</h1>
+          <form>
+            <div className='flex flex-row md:flex-col lg:flex-row gap-2'>
+              <input type='text' placeholder='Name*' />
+              <input type='text' placeholder='Email*' />
+            </div>
+            <div className='flex flex-row md:flex-col lg:flex-row gap-2'>
+              <input type='text' placeholder='Subject*' />
+              <input type='text' placeholder='Phone' />
+            </div>
+            <textarea className='w-full h-10 border resize-none outline-none' />
+          </form>
         </div>
-        <div className='w-1/2'>
+        <div className='w-full md:w-1/2 py-4'>
           <h2 className='text-black text-2xl font-bold py-2'>Looking Forward to Hear From You</h2>
           <p className='text-black text-md py-2'>Contact sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <ContactInfo fontStyle='text-black' />
